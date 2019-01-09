7629007c6f30ef49c506ac7c6a8b67519bcae017
sidinsomniac <chatterjeesid1993@gmail.com>
2019-01-09 17:58:26 +0530

    Use blog api methods

--- src/app/blog-view/blog-view.component.ts
+++ src/app/blog-view/blog-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BlogHttpService } from '../blog-http.service';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-blog-view',
   templateUrl: './blog-view.component.html',
@@ -10,11 +11,44 @@ export class BlogViewComponent implements OnInit {
   public currentBlog = [];
   public errorMsg;
 
-  constructor( private _route: ActivatedRoute, private router: Router, public blogHttpService:BlogHttpService) { }
+  newBlogId:string;
+
+  constructor(private _route: ActivatedRoute, private router: Router, private blogHttpService: BlogHttpService, private location: Location) { }
 
   ngOnInit() {
     let myBlogId = this._route.snapshot.paramMap.get('blogId');
+    this.newBlogId = myBlogId;
     this.blogHttpService.getSingleBlogInformation(myBlogId)
-      .subscribe( data => this.currentBlog = data['data'] , error => this.errorMsg = error );
-  }  
+      .subscribe(data => this.currentBlog = data['data'], error => this.errorMsg = error);
+  }
+
+  deleteThisBlog(): any {
+
+    this.blogHttpService.deleteBlog(this.newBlogId).subscribe(
+
+      data => {
+        console.log(data);
+        setTimeout(() => {
+          this.router.navigate(['/home']);
+        }, 1000)
+
+      },
+      error => {
+        console.log("some error occured");
+        console.log(error.errorMessage);
+      }
+
+
+    )
+
+
+
+
+  }// end delete this blog 
+
+  goBackToPreviousPage(): any {
+
+    this.location.back();
+
+  }
 }
